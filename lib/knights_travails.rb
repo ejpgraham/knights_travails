99803816c05943b88f17764484e1d644563dbd08
JamesBallEdwardGraham <appacademy@Apps-Mac-mini-2.local>
2017-03-03 15:48:48 -0500

    Add #build_move_tree

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,8 +1,12 @@
+require_relative "00_tree_node"
+
 class KnightPathFinder
+  attr_reader :root
 
   def initialize(start_pos)
     @start_pos = start_pos
     @visited_positions = [start_pos]
+    @root = PolyTreeNode.new(@start_pos)
   end
 
   def self.valid_moves(pos)
@@ -39,12 +43,30 @@ class KnightPathFinder
   end
 
   def build_move_tree
+    # root = PolyTreeNode.new(@start_pos)
+    queue = [root]
+
+    until queue.empty?
+      current_node = queue.shift
+      moves = self.new_move_positions(current_node.value)
+
+      moves.each do |pos|
+        child_node = PolyTreeNode.new(pos)
+        child_node.parent = current_node
+        queue << child_node
+      end
+    end
   end
 
   def find_path
   end
 
-end
 
-p KnightPathFinder.valid_moves([4,4])
-p KnightPathFinder.valid_moves([0,0])
+
+end
+#
+# p KnightPathFinder.valid_moves([4,4])
+# p KnightPathFinder.valid_moves([0,0])
+k = KnightPathFinder.new([0,0])
+k.build_move_tree
+p k.root
